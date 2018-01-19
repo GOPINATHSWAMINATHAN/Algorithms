21c180506466e5625fef8edc53873b5d9201f2f1
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2018-01-19 21:54:13 +0300

    Initial Commit

--- src/PowerOfTwo.java
+++ src/PowerOfTwo.java
@@ -4,17 +4,17 @@ public class PowerOfTwo {
         if (n < 1) {
             return 0;
         } else if (n == 1) {
-            System.out.println("else if "+1);
+            System.out.println(1);
             return 1;
         } else {
             int prev = powersOf2(n / 2);
             int curr = prev * 2;
-            System.out.println("else "+curr);
+            System.out.println(curr);
             return curr;
         }
     }
 
     public static void main(String[] args) {
-        powersOf2(4);
+        powersOf2(8);
     }
 }
