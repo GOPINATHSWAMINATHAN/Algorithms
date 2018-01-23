9267d7b6102610185e6b6438a62fb03a6604c211
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2018-01-23 22:02:28 +0300

    Initial Commit

--- src/InsertNode.java
+++ src/InsertNode.java
@@ -22,4 +22,37 @@ public class InsertNode {
         head = newNode;
     }
 
-}
+    public void insertAfter(Node prev_node, int new_data) {
+        if (prev_node == null) {
+            System.out.println("Previous node cannot be null");
+            return;
+        }
+
+        Node new_node = new Node(new_data);
+        //Creating new node to the next of previous node.
+        new_node.next = prev_node.next;
+
+        //Assigning the value to the current node which is next node of previous node.
+        prev_node.next = new_node;
+    }
+
+    public void append(int new_data)
+    {
+        Node new_node=new Node(new_data);
+        if(head==null)
+        {
+            head=new Node(new_data);
+            return;
+        }
+
+        new_node.next=null;
+        Node last=head;
+        while(last.next!=null)
+            last=last.next;
+
+            last.next=new_node;
+            return;
+        }
+    }
+
+
