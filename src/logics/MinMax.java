cda7abda81f4e6cdf92474ccc5b59606c8a154f2
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2018-02-21 12:32:33 +0300

    Initial Commit

---
+++ src/logics/MinMax.java
@@ -0,0 +1,15 @@
+package logics;
+
+public class MinMax {
+
+    public static void main(String[] args) {
+        int min=Integer.MIN_VALUE;
+        int max=Integer.MAX_VALUE;
+int sum=0;
+       for(int i=0;i<max;i++)
+       {
+           sum+=i;
+       }
+        System.out.println(sum);//1073741825
+    }
+}
