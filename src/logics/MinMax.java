152d0a67366253153dda0bb81d04bf60e7a12ec1
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2018-02-21 14:31:35 +0300

    Initial Commit

--- src/logics/MinMax.java
+++ src/logics/MinMax.java
@@ -4,11 +4,24 @@ public class MinMax {
 
     public static void main(String[] args) {
         int min = Integer.MIN_VALUE;
-        int max = Integer.MAX_VALUE;
-        int sum = 0;
-        for (int i = 0; i < max; i++) {
-            sum += i;
-        }
-        System.out.println(sum);//1073741825
+//        int max = Integer.MAX_VALUE;
+//        int sum = 0;
+//        for (int i = 0; i < max; i++) {
+//            sum += i;
+//        }
+//        System.out.println(sum);//1073741825
+        int a = f(4);
+        System.out.println(a);
     }
+
+    //Recursive Runtimes
+
+    static int f(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return f(n - 1) + f(n - 1);
+
+    }
+
 }
